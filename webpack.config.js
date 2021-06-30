d340b9852092effaa9917d9c18e4e2c85f895867
Francisco <francisco.marquez.solt@gmail.com>
2021-06-30 14:41:17 -0500

    Added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path"); /* path ya esta disponible en node */
+
+module.exports = {
+  entry:
+    "./src/index.js" /* Definimos el punto de entrada, por defecto es index.js */,
+  output: {
+    path: path.resolve(
+      __dirname,
+      "dist"
+    ) /* Usamos resolve para obtener automaticamente la direccion de nuestro directorio donde estamos trabajando */,
+    filename: "main.js",
+  } /* Por defecto el punto de salida es dist, pero lo podemos cambiar */,
+  resolve: {
+    extensions: [".js"], /* Especificamos las extensiones de archivo con los que vamos a trabajar */
+  }, 
+};
