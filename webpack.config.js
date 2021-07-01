d24f718fe8bf91d597ee9458fb5fc39617e0c1a6
Francisco <francisco.marquez.solt@gmail.com>
2021-07-01 15:06:23 -0500

    Html configurated to webpack

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path"); /* path ya esta disponible en node */
+const HtmlWebpackPlugin = require("html-webpack-plugin"); /* Requerimos un comando */
+
+module.exports = {
+  entry:
+    "./src/index.js" /* Definimos el punto de entrada, por defecto es index.js */,
+  output: {
+    path: path.resolve(
+      __dirname,
+      "dist"
+    ) /* Usamos resolve para obtener automaticamente la direccion de nuestro directorio donde estamos trabajando */,
+    filename: "main.js",
+  } /* Por defecto el punto de salida es dist, pero lo podemos cambiar */,
+  resolve: {
+    extensions: [
+      ".js",
+    ] /* Especificamos las extensiones de archivo con los que vamos a trabajar */,
+  },
+  module: {
+    rules: [
+      /* Modulos son como extensiones */
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "./public/index.html",
+      filename: "./index.html",
+    }) /* Con esto generaremos un archivo HTML con un punto de entrada (template) en el dist */,
+  ],
+};
